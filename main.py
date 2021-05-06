6dca59066283ec2ec4a98e73a2d673dd8d221888
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-05-06 22:52:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,341 @@
+# Importing required modules
+
+import streamlit as st
+from streamlit_folium import folium_static
+import folium as f,folium
+import osmnx as ox
+import networkx as nx
+import geopy
+import geopandas
+import requests
+import json
+# import os
+# import base64
+# import taxicab as tc
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# import plotly.graph_objs as go
+# import numpy as np
+# from folium import IFrame
+
+
+
+#************************  MAIN WEB-APPLICATION STARTS HERE   ***********************************************#
+
+# Main application starts
+
+# Display image at top
+st.image('./map.jpg')
+
+# Display the Title and header of the application
+st.title("Shortest Route for Electric Vehicles")
+st.header('Using Python OSMNX and Folium Library')
+st.write(' ')
+
+# First Selectbox to select the input method by the users
+input_method = st.sidebar.selectbox("Select the Input method",("Coordinates", " Place Name"))
+
+# Second Selectbox to select the network type of the graph by the users
+network_value = st.sidebar.selectbox("Select the Network type",("Walk","Drive","Bike","All"))
+
+# Changing the network_value to the suitable format according to the selection by the user
+if network_value=='Walk':
+    network_value='walk'
+elif network_value=='Drive':
+    network_value='drive'
+elif network_value=='Bike':
+    network_value='bike'
+else:
+    network_value='all'
+    
+# Third Selectbox to select the algorithm to be used for the calculation of shortest_path
+algo_type = st.sidebar.selectbox("Select the Algorithm type",("Dijkstra’s Algorithm","Bellman-Ford Algorithm"))
+
+# Changing the algo_type to the suitable format according to the selection by the user
+if algo_type=='Dijkstra’s Algorithm':
+    algo_value='dijkstra'
+else:
+    algo_value='bellman-ford'
+
+# Initialising four variables input1,input2,input3,input4 to store the value of the coordinates(lattitude,longitude)
+# of source and destination respectively.
+input1 = 0.0000   # source's lattitude
+input2 = 0.0000   # source's longitude
+input3 = 0.0000   # destination's lattitude
+input4 = 0.0000   # destination's longitude
+
+
+# if the user selected the input_method as coordinates
+if input_method=='Coordinates':
+
+    # Display text
+    st.write("""
+    Enter the coordinate of the Source
+    """)
+
+    # Activated two columns for input of lattitude and longitude of source
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        input1 = st.number_input('Source Lattitude Coordinates')
+    with col2:
+        input2 = st.number_input('Source Longitude Coordinates')
+
+    # Display the stored values of input by the user to double-check the given inputs
+    st.write("Lattitude:",input1,"Longitude:", input2)
+
+    # Display text
+    st.write("""
+    Enter the coordinate of the Destination
+    """)
+
+    # Activated two columns for input of lattitude and longitude of destination
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        input3 = st.number_input('Destination Lattitude Coordinates')
+    with col2:
+        input4 = st.number_input('Destination Longitude Coordinates')
+
+    # Display the stored values of input by the user to double-check the given inputs
+    st.write("Lattitude:",input3,"Longitude:", input4)
+
+    # A Radio button with three options to select the map_type by the user 
+    map_type = st.radio(
+    'Select map type',
+    ['OpenStreetMap', 'CartodbPositron','StamenTerrain']
+    )
+
+    # Slider to select the distance range between source and destination by the user ( in Kms )
+    range = st.select_slider('Select distance range ( in Km )', options=[10,'20','30','40','50','60','70','80','90','100'])
+
+    # Converting the selection of the user in integer from string, and changing it into metres
+    range_value=int(range)
+    range_value*=1000
+
+    # Display the input in metres to double-check the given inputs
+    st.write("Distance range in metres:",range_value)
+
+    # When the button is clicked, it will return a true boolean value 
+    # which makes the inside code of if statement to execute
+    if st.button('Show me the Way'):
+
+        # try and except blocks to handle the errors during the process of generation of maps with shortest routes
+        try:
+            # Configure OSMnx by setting the default global settings’ values
+            ox.config(use_cache=True, log_console=True)
+
+            # Creating a graph from OSMNX within some distance of source point
+            G = ox.graph_from_point((input1, input2),dist=range_value,network_type=network_value,simplify=False)
+            
+            # adding edge speeds for all the nodes present in the graph G
+            G = ox.speed.add_edge_speeds(G)
+
+            # adding edge travel times for all the nodes present in the graph G
+            G = ox.speed.add_edge_travel_times(G)
+
+            # getting the nearest node from the source coordinates in the graph G generated above
+            orig = ox.get_nearest_node(G, (input1, input2))
+
+            # getting the nearest node from the destination coordinates in the graph G generated above
+            dest = ox.get_nearest_node(G, (input3, input4))
+
+            # shortest_path() function generates the shortest path between the orgin and source
+            # and store the path in a 'route' named variable according to the algorithm
+            # selected by the user and all computations are based on travel time
+            route = nx.shortest_path(G, orig, dest, 'travel_time',method=algo_value)
+
+            # Plotting the route on the folium map named 'route_map'
+            route_map = ox.plot_route_folium(G, route,tiles=map_type)
+
+            # Putting a blue folium Marker on the source coordinates with proper tooltip
+            tooltip1 = "Source"
+            folium.Marker(
+            [input1, input2], popup="Source", tooltip=tooltip1,icon=folium.Icon(color='blue')).add_to(route_map)
+
+            # Putting a red folium Marker on the destination coordinates with proper tooltip
+            tooltip2 = "Destination"
+            folium.Marker(
+            [input3, input4], popup="Destination", tooltip=tooltip2,icon=folium.Icon(color='red')).add_to(route_map)
+
+            # Displaying the final map with shortest_path plotted and markers placed on the correct locations
+            folium_static(route_map)
+        
+        # if some error occurs during the generation of maps, then this except block will execute
+        except:
+
+            # Displaying the error message with some suggestions
+            st.image('./error.png')
+            st.error('Sorry ! No Graph exists for the given inputs.')
+            st.text("Please try again with some other input values.")
+            
+        
+# if the user selected the input_method as Place name
+else:
+
+    # Display text
+    st.write("""
+    Enter the name of the Source
+    """)
+
+    # take source as input
+    place1 = st.text_input('Source')
+
+    # Replacing the blankspaces in the source input string with '+' character 
+    # to make it properly formatted to make API call using Google Geocoding API
+    place1=place1.replace(" ","+")
+    st.write('')
+
+    # Display text
+    st.write("""
+    Enter the name of the Destination
+    """)
+
+    # take destination as input
+    place2 = st.text_input('Destination')
+
+    # # Replacing the blankspaces in the destination input string with '+' character 
+    # to make it properly formatted to make API call using Google Geocoding API
+    place2=place2.replace(" ","+")
+
+    # A Radio button with three options to select the map_type by the user
+    map_type = st.radio(
+    'Select map type',
+    ['OpenStreetMap', 'CartodbPositron','StamenTerrain']
+    )
+
+    # Slider to select the distance range between source and destination by the user ( in Kms )
+    range = st.select_slider('Select distance range ( in Km )', options=[10,'20','30','40','50','60','70','80','90','100'])
+
+    # Converting the selection of the user in integer from string, and changing it into metres
+    range_value=int(range)
+    range_value*=1000
+
+    # Display the input in metres to double-check the given inputs
+    st.write("Distance range in metres:",range_value) 
+
+    # When the button is clicked, it will return a true boolean value 
+    # which makes the inside code of if statement to execute
+    if st.button('Show me the Way'):
+
+        # try and except blocks to handle the errors during the API calls and data retreival from Google Geocoding API.
+        try:
+            # url variable store urL ( to make use of Google Geocoding API later )
+            url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+            # enter your api key here ( already generated one from Google Cloud Platform )
+            api_key='AIzaSyC3S0N05MSKjXr9PgN5_YSFX2WTHjSiA1w'
+
+            # get method of requests module returns response object
+            # API Call to get the response json for the source
+            res_ob1 = requests.get(url+'address='+place1+'&key='+api_key)
+
+            # json method of response object converts json format data into python format data
+            results1 = res_ob1.json()['results']
+            my_geo1 = results1[0]['geometry']['location']
+
+            # Display the proper name of the Source according to the response received from the API call
+            source_name_full = results1[0]['formatted_address']
+            st.write("Source: ",source_name_full)
+
+            # Display the retrieved lattitude and longitude of source from the API call
+            st.write("Latitude:",my_geo1['lat'],"\n","Longitude:",my_geo1['lng'])
+            input1 = my_geo1['lat']
+            input2 = my_geo1['lng']
+
+
+            # get method of requests module returns response object
+            # API Call to get the response json for the destination
+            res_ob2 = requests.get(url+'address='+place2+'&key='+api_key)
+
+            # json method of response object converts json format data into python format data
+            results2 = res_ob2.json()['results']
+            my_geo2 = results2[0]['geometry']['location']
+
+            # Display the proper name of the Destination according to the response received from the API call
+            dest_name_full= results2[0]['formatted_address']
+            st.write("Destination: ",dest_name_full)
+
+            # Display the retrieved lattitude and longitude of destination from the API call
+            st.write("Latitude:",my_geo2['lat'],"\n","Longitude:",my_geo2['lng'])
+            input3 = my_geo2['lat']
+            input4 = my_geo2['lng']
+
+            # try and except blocks to handle the errors during the process of generation of maps with shortest routes
+            try:
+                # Configure OSMnx by setting the default global settings values
+                ox.config(use_cache=True, log_console=True)
+
+                # Creating a graph from OSMNX within some distance of source point
+                G = ox.graph_from_point((input1, input2),dist=range_value,network_type=network_value,simplify=False)
+
+                # # adding edge speeds for all the nodes present in the graph G
+                G = ox.speed.add_edge_speeds(G)
+
+                # # adding edge travel times for all the nodes present in the graph G
+                G = ox.speed.add_edge_travel_times(G)
+
+                # getting the nearest node from the source coordinates in the graph G generated above
+                orig = ox.get_nearest_node(G, (input1, input2))
+
+                # getting the nearest node from the destination coordinates in the graph G generated above
+                dest = ox.get_nearest_node(G, (input3, input4))
+
+                # shortest_path() function generates the shortest path between the orgin and source
+                # and store the path in a 'route' named variable according to the algorithm
+                # selected by the user and all computations are based on travel time
+                route = nx.shortest_path(G, orig, dest, 'travel_time',method=algo_value)
+
+                # Plotting the route on the folium map named 'route_map'
+                route_map = ox.plot_route_folium(G, route,tiles=map_type)
+
+                # Putting a blue folium Marker on the source coordinates with proper tooltip
+                tooltip1 = "Source"
+                folium.Marker(
+                [input1, input2], popup="Source", tooltip=tooltip1,icon=folium.Icon(color='blue')).add_to(route_map)
+
+                # Putting a red folium Marker on the destinaton coordinates with proper tooltip
+                tooltip2 = "Destination"
+                folium.Marker(
+                [input3, input4], popup="Destination", tooltip=tooltip2,icon=folium.Icon(color='red')).add_to(route_map)
+
+                # Displaying the final map with shortest_path plotted and markers placed on the correct locations
+                folium_static(route_map)
+
+             # if some error occurs during the generation of maps, then this except block will execute 
+            except:
+                
+                # Displaying the error message with some suggestions
+                st.image('./error.png')
+                st.error('Sorry ! No Graph exists for the given inputs.')
+                st.text("Please try again with some other input values.")
+
+        # if some error occurs in making API calls, then this except block will execute
+        except:
+
+            # Displaying the error message and possible reasons
+            st.error("Error ! Could not fetch the coordinates of the given inputs.")
+            st.write("""
+            Possible Reasons:\n
+            1) API Key is invalid or not active.\n 
+            2) Input is invalid.
+            """)
+
+
+
+#***************************************** WEB-APPLICATION ENDS HERE  ************************************#
+
+        
+            
+        
+
+    
+
+
+
+
+
+
+
+
+
+
+
